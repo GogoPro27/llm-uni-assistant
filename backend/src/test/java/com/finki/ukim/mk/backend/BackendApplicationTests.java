63222dda53b2b6d86964017d0f9dc3195a3d900d
Gorazd <fi.gorazd@gmail.com>
2025-08-23 21:54:07 +0200

    Initial commit, initialize backend

---
+++ backend/src/test/java/com/finki/ukim/mk/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.ukim.mk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
